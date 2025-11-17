4abcb8f064315354f21b3ed4729f70d764047462
Areeza2660 <s235641@dtu.dk>
2025-11-17 15:54:38 +0100

    Update Program.cs

--- aflevering2/Program.cs
+++ aflevering2/Program.cs
@@ -4,7 +4,6 @@ Console.WriteLine("How many motors are carrying the packages?");
 var input = Console.ReadLine();
 var motors = int.Parse(input);
 
-
 Console.WriteLine("How many kg of packages do we expect?");
 double kg = Convert.ToDouble(Console.ReadLine());
 
